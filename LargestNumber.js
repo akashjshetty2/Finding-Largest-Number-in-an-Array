8d8fbca483fe45104491cd1b4bb53ab316140632
Akash J Shetty <97341311+akashjshetty2@users.noreply.github.com>
2026-03-09 13:15:59 +0530

    Update LargestNumber.js

--- LargestNumber.js
+++ LargestNumber.js
@@ -1,1 +1,16 @@
+function getLargestNumber(arr){
+if (arr.length===0){
+  throw new ('The array has no element')
+}
+  let largest=arr[0]
 
+  for(let i=1;i<arr.length;i++){
+    if(arr[i]>largest){
+      largest=arr[i]
+    }
+  }
+return largest;
+}
+const numbers=[3,87,56,97,42,6]
+const largestElement=getLargestNumber(numbers)
+console.log('The largest element in an array is: ' + largestElement)
